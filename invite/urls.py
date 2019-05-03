a718bb22724e11bed5ccbb2491bc8266782246ac
watibini <watimakhanu@gmail.com>
2019-05-04 00:45:32 +0300

    page redirection done

--- invite/urls.py
+++ invite/urls.py
@@ -1,11 +1,25 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
+from allauth.account.views import confirm_email, login, logout
+import importlib
+from allauth.socialaccount import providers
+
+providers_urlpatterns = []
+
+for provider in providers.registry.get_list():
+    prov_mod = importlib.import_module(provider.get_package() + '.urls')
+    providers_urlpatterns += getattr(prov_mod, 'urlpatterns', [])
 
 urlpatterns=[
-    url(r'^invite$',views.index,name = 'index'),
-    url(r'',views.invite,name='invite'),
+    url(r'^index$',views.index,name = 'index'),
+    # url(r'',views.invite,name='invite'),
+    url(r'^auth/', include(providers_urlpatterns)),
+    url(r'^confirm-email/(?P<key>[-:\w]+)/$', confirm_email, name='account_confirm_email'),
+    url(r'^login/$', login, name='account_login'),
+    url(r'^logout/$', logout, name='account_logout'),
+    url(r'^signup/$', login, name='account_signup'),  # disable email signup
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
